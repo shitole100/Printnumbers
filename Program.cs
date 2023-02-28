0681abb2068928d9a07e14a95e8f8b6fbf1ce500
Admin <Admin@DESKTOP-0C3M1BG>
2023-02-28 22:00:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+public class programm
+{ 
+
+    public static void Main()
+    {
+        printnumbers();
+
+
+    }
+
+    public static void printnumbers()
+    {
+        // print numbers
+       // for (int i = 0; i <=10; i++)
+       // {
+            //Console.WriteLine(i);
+        // }
+
+        // print numbers
+         for (int i = 5; i >=0; i--)
+         {
+            {
+                for (int j = i; j >=0; j--)
+                {
+
+                    Console.Write(i);
+                }
+
+
+
+            }
+
+                Console.WriteLine();
+
+
+         }
+
+            
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
